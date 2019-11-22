e66ae43e047f5200f21aa43f5e918adeeba97c10
calmevtime <calmevtime@gmail.com>
2019-11-22 13:17:29 -0700

    first commit

--- classification/scripts/resnet_upscaled_static.sh
+++ classification/scripts/resnet_upscaled_static.sh
@@ -2,4 +2,4 @@
 subset=$1
 subset=${subset:-0}
 echo "subset: $subset"
-python main/imagenet_resnet_upscaled_static.py -j 16 --gpu-id 0,1,2,3 --arch ResNetDCT_Upscaled_Static --subset $subset --data ./ILSVRC2012
+python main/imagenet_resnet_upscaled_static.py -j 16 --gpu-id 0,1,2,3 --arch ResNetDCT_Upscaled_Static --subset $subset --resume pretrained/resnet50dct_upscaled_static_$subset/model_best.pth.tar --data ./ILSVRC2012
